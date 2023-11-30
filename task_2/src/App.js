6928750be8fc4a076eacee3ae818e0aaa9c55bc1
Satya7816 <114287094+Satya7816@users.noreply.github.com>
2023-11-30 09:01:59 +0530

    Add files via upload

---
+++ task_2/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Header from './components/Header';
+import Spaceagency from './components/spaceagency';
+import Community from './components/community';
+import Footer from './components/Footer';
+import Cards from './components/cards';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Spaceagency/>
+    <Community/>
+    <Footer/>
+    <Cards/>
+    </>
+  );
+}
+
+export default App;
